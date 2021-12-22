38a7d5738e9fbb73cc526d837b166ddd9f723b08
unknown <spanxscomercial@gmail.com>
2021-12-22 20:51:38 -0300

    Clear atualização

--- src/commands/moderação/clear.js
+++ src/commands/moderação/clear.js
@@ -6,34 +6,34 @@ module.exports = class extends Command {
             name: 'clear',
             description: 'Apaga as mensagens de um canal!',
             options: [{
-                name: 'canal',
-                description: 'Coloque o canal a ser apagado',
-                type: 'CHANNEL',
-                required: true
-            },
-            {
                 name: 'quantidade',
-                description: 'Coloque as mensagens a serem apagadas, de 1 à 100',
-                type: 'NUMBER',
+                type: 'STRING',
+                description: 'Quantas mensagens deseja apagar?',
                 required: true,
-            },
+            }
         ]
         })
     }
 
     run = async (interaction) => {
         
-        interaction.channel.sendTyping()
+        try {
+            if(!interaction.member.permissions.has("MANAGE_MESSAGES")) return interaction.followUp({content: `Você não tem permissão para isso!`});
+            if(!interaction.guild.me.permissions.has("MANAGE_MESSAGES")) return interaction.followUp({content: `Eu não tenho permissão para isso!`});
+            let delamount = interaction.options.getString('quantidade');
+            if (isNaN(delamount) || parseInt(delamount <= 0)) return interaction.followUp({content: ' > ``Coloque um número específico;``\n > ``Exp: /clear 3.``'} )
 
-        let quan = interaction.options.getNumber('quantidade');
-        let channel = interaction.options.getChannel('canal');
+            if (parseInt(delamount) > 100) return interaction.followUp({content:'Só podes eleminar 99 mensagens de uma vez!'})
 
-        if (channel.type !== 'GUILD_TEXT') return interaction.reply({ content: 'O canal dito não é um canal de texto', ephemeral: true })
+            interaction.followUp('``Clear feito com sucesso!``✅').then(m => {
+                setTimeout(() => {
+                  interaction.channel.bulkDelete(parseInt(delamount) * 1, true)
+                }, 1000) // 2 segundos
+            })
 
-        if (!quan || quan < 1 || quan > 100) return interaction.reply({ content: "Você precisa escolher entre apagar 1 à 100!", ephemeral: true });
-
-        channel.bulkDelete(quan)
-
-        interaction.reply({ content: `Foi deletado ${quan} mensagens `})
+          
+        } catch (e) {
+            console.log(e)
+        }
     }
 }
