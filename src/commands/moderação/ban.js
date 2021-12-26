9a7f4dc5f475d1025fd9ed71e22b2077eca7c3ab
unknown <spanxscomercial@gmail.com>
2021-12-26 16:33:15 -0300

    Ban atualizaado

--- src/commands/moderação/ban.js
+++ src/commands/moderação/ban.js
@@ -24,16 +24,14 @@ module.exports = class extends Command {
     }
 
     run = (interaction) => {
-
+        try {
         //interaction.reply({ content: "Em criação", ephemeral: true })
 
         let user = interaction.options.getUser("usuario");
         const memberTarget = interaction.guild.members.cache.get(user.id);
-        let rr = interaction.options.getString("motivo");
         let rra = interaction.options.getString("motivo");
 
         if(rra == null) rra = `Sem motivo`;
-        if(rr == null) rr = `Banido pelo dono do server!`;
 
         if(user.id == interaction.user.id) return interaction.followUp({ content: `Você não pode se banir!`, epehemral: true });
         if(user.id == interaction.guild.me.id) return interaction.followUp({content: `Você não pode me banir!`, ephemeral: true });
@@ -43,21 +41,16 @@ module.exports = class extends Command {
         return interaction.reply({ content: "Você não tem permissão para usar esse comando!", epehemeral: true });
 
         if(!interaction.guild.me.permissions.has("BAN_MEMBERS"))
-        return interaction.reply({ content: "Eu não tenho permissão para banir usuários!", epehemeral: true });
+        return interaction.reply({ content: "Eu não tenho permissão para banir usuários!", epehemeral: true })
 
-        if(interaction.guild.ownerId) {
-            interaction.followUp({content: `${user} foi banido! Motivo: ` +  rr })
-            
-            memberTarget.ban({reason: rr || `Banido pelo dono do server!`})
-           
-        } else {
-        interaction.followUp({content: `${user} foi banido! Motivo: ` +  rra })
+
+        interaction.reply({content: `${user} foi banido! Motivo: ` +  rra })
 
         memberTarget.ban({reason: rra})
 
+        } catch(e) {
+            console.log(e)
         }
-
-
 
 
     }
